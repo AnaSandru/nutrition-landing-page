00cf409574532a3c7f24786a8584d84f52894d99
Sandru Ana-Maria <ana-maria.sandru@bp.com>
2026-02-08 21:18:54 +0200

    Updates

--- _next/static/hgWI4i9e2PGJsMZEbuP1f/_buildManifest.js
+++ _next/static/hgWI4i9e2PGJsMZEbuP1f/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/landing-nutrition//_next/:path+",
+        "destination": "/landing-nutrition/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
